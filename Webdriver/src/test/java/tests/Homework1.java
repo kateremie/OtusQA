a541ae3009e5a6630df273fcacfefb15ce2bd6a7
kateremie <morgausa@rambler.ru>
2018-04-02 22:42:56 +0300

    Fixed.

--- Webdriver/src/test/java/tests/Homework1.java
+++ Webdriver/src/test/java/tests/Homework1.java
@@ -16,25 +16,33 @@ public class Homework1 {
 
     public void test()throws InterruptedException{
        WebDriver driver = new ChromeDriver();
+
        //1. Открыть страницу BlazeDemo.com
        driver.get("http://blazedemo.com/");
+
        //задаём параметры поиска
        String Depart = new String("Boston");
        String Arrive = new String("London");
+
        //закидываем соответствующий параметр в поле departure city
        WebElement fromPort = driver.findElement(By.cssSelector("select[name='fromPort']"));
        fromPort.sendKeys(Depart);
+
        //закидываем соответствующий параметр в поле destination city
        WebElement toPort = driver.findElement(By.cssSelector("select[name='toPort']"));
        toPort.sendKeys(Arrive);
+
        //нажимаем кнопку Find Flights
        driver.findElement(By.cssSelector("input[type='submit']")).click();
+
        //сравниваем параметр поиска Depart с соответствующим параметром поисковой выдачи
        String DepartFromTable = new String(driver.findElement(By.cssSelector("div>input[name='fromPort']")).getAttribute("value"));
        Assertions.assertEquals(Depart,DepartFromTable);
+
        //сравниваем параметр поиска Arrive с соответствующим параметром поисковой выдачи
        String ArriveFromTable = new String(driver.findElement(By.cssSelector("div>input[name='toPort']")).getAttribute("value"));
        Assertions.assertEquals(Arrive,ArriveFromTable);
+
        //проверяем, что результат поиска не пуст - отображается кнопка Choose This Flight
        WebElement ChooseFlightButton = driver.findElement(By.cssSelector("input[type=submit]"));
        assertTrue(ChooseFlightButton.isDisplayed());
