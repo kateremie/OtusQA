34f7e8201b6718c1ea110058eb383ef13228e469
kateremie <morgausa@rambler.ru>
2018-06-04 13:59:00 +0300

    Fixed

--- JDIblazedemoTest/src/main/java/blazedemo2/forms/FlightForm.java
+++
@@ -1,7 +0,0 @@
-package blazedemo2.forms;
-
-import blazedemo2.models.FlightModel;
-import com.epam.jdi.uitests.web.selenium.elements.composite.Form;
-
-public class FlightForm extends Form<FlightModel> {
-}
--- JDIblazedemoTest/src/main/java/blazedemo2/models/FlightModel.java
+++
@@ -1,4 +0,0 @@
-package blazedemo2.models;
-
-public class FlightModel {
-}
